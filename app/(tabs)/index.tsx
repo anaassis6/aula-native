472dbac49802992354c6d195b657d37c9c13714b
anaassis6 <analuassis6@gmail.com>
2025-08-01 14:13:12 -0300

    arrumando o alert

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -41,7 +41,7 @@ export default function TabOneScreen() {
       />
       <Button
         title='ENVIAR'
-        onPress={() => { alert(nome + '/n ' + sobrenome + '/n ' + cpf + '/n ' + telefone + '/n ' + email) }}
+        onPress={() => { alert(nome + ' ' + sobrenome + ' ' + cpf + ' ' + telefone + ' ' + email) }}
 
       />
     </View>
