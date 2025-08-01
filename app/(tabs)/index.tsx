3c731f1dc9aef30bed8abe69c09501eb49977ac4
anaassis6 <analuassis6@gmail.com>
2025-08-01 13:56:28 -0300

    atividade #01

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,49 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet, Image, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 import { Text, View } from '@/components/Themed';
+import { sortRoutes } from 'expo-router/build/sortRoutes';
 
 export default function TabOneScreen() {
+  const [nome, atualizaNome] = useState('Nome');
+  const [sobrenome, atualizaSobrenome] = useState('Sobrenome');
+  const [cpf, atualizaCpf] = useState('CPF');
+  const [email, atualizaEmail] = useState('Email');
+  const [telefone, atualizaTelefone] = useState('Telefone');
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Insira seus dados</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image
+        source={require('@/assets/images/woman-icon.png')}
+      />
+      <TextInput
+        value={nome}
+        onChangeText={atualizaNome}
+      />
+      <TextInput
+        value={sobrenome}
+        onChangeText={atualizaSobrenome}
+      />
+      <TextInput
+        value={cpf}
+        onChangeText={atualizaCpf}
+        inputMode='numeric'
+      />
+      <TextInput
+        value={telefone}
+        onChangeText={atualizaTelefone}
+        inputMode='numeric'
+      />
+      <TextInput
+        value={email}
+        onChangeText={atualizaEmail}
+        inputMode='email'
+      />
+      <Button
+        title='ENVIAR'
+        onPress={() => { alert(nome + '/n ' + sobrenome + '/n ' + cpf + '/n ' + telefone + '/n ' + email) }}
+
+      />
     </View>
   );
 }
